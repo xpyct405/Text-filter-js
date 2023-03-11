90eba43aa280b05e16eca7e5920a03cab3568bf7
Rustam Kuzyaev <xpyct405@yandex.ru>
2023-03-11 10:18:53 +0300

    feat: add description of code

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-function myFunction(){
-
 let input = document.querySelector("#myInput");
-let filter = input.value.toUpperCase(); //Запоминает всё что пишем в Инпут
 let ul = document.querySelector("#myUL");
 let li = ul.querySelectorAll("li");
 
-console.log(input);
-console.log(filter);
-console.log(ul);
-console.log(li);
+function myFunction(){
+
+let filter = input.value.toUpperCase(); //Запоминает всё что пишем в input
+// console.log(filter);
 
     for (let i = 0; i < li.length; i++){
         let a = li[i].querySelectorAll("a")[0];
-        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        // console.log(a.innerHTML); // выдаёт все значения городов
+        
+        if (a.innerHTML.toUpperCase().indexOf(filter) >= 0) {
             li[i].style.display = "";
-            console.log(a.innerHTML); //Если ничего не написано выдаёт все значения, если пишешь буквы filter запомниает их и выдаёт все слова где есть это буква
+            console.log(a.innerHTML); // выдаёт все значения городов в которых есть буква введеная в input
+            console.log(a.innerHTML.toUpperCase().indexOf(filter)); // индекс буквы в слове
+            // console.log(a.innerHTML); //Если ничего не написано выдаёт все значения, если пишешь буквы filter запомниает их и выдаёт все слова где есть это буква
+            
         } else {
             li[i].style.display = "none";
         }
